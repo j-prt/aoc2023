e8dc53230cfee9465bb1d69d6692e007b5507c51
j-prt <peart.l.joe@gmail.com>
2023-12-05 17:48:13 -0800

    solve day 5 part 2: the brutest

--- day5/solution5.py
+++ day5/solution5.py
@@ -2,9 +2,6 @@
 
 # Puzzle 1
 
-with open('example.txt') as f:
-    example = f.read()
-
 def split_sections(inputs):
     split = inputs.split('\n\n')
 
@@ -42,10 +39,76 @@ def solve(inputs):
     print(min_seed)
 
 # Test
-solve(example)
-
-with open('input5.txt') as f:
-    inputs = f.read()
+# with open('example.txt') as f:
+#     example = f.read()
+# solve(example)
 
 # Solution
-solve(inputs)
+# with open('input5.txt') as f:
+#     inputs = f.read()
+# solve(inputs)
+
+
+# Puzzle 2
+#
+# AKA
+#
+# 'Waiting is Eaiser Than Thinking'
+#               or
+# 'Multiprocessing isn't THAT Hard'
+
+from multiprocessing import Process, SimpleQueue, cpu_count
+
+def worker(jobs, results):
+    while data := jobs.get():
+        seed_range, mappings = data
+        min_seed = float('inf')
+        for seed in seed_range:
+            for mapping in mappings:
+                seed = map_resolver(seed, mapping)
+            min_seed = seed if seed < min_seed else min_seed
+        print('Worker finished one')
+        results.put(min_seed)
+    results.put(0)
+
+def start_jobs(seeds, mappings, procs, jobs, results):
+    for seed_range in seeds:
+        jobs.put((seed_range, mappings))
+    for _ in range(procs):
+        proc = Process(target=worker, args=(jobs, results))
+        proc.start()
+        jobs.put(0)
+
+def report(procs, results):
+    procs_done = 0
+    min_val = float('inf')
+    while procs_done < procs:
+        val = results.get()
+        if val == 0:
+            procs_done += 1
+        else:
+            print('report val:', val)
+            if val < min_val:
+                min_val = val
+    return min_val
+
+def solve_mp(inputs):
+    procs = cpu_count()
+    jobs = SimpleQueue()
+    results = SimpleQueue()
+
+    seeds, mappings = split_sections(inputs)
+    seed_ranges = []
+    for i in range(0, len(seeds), 2):
+        seed_range = range(seeds[i], seeds[i]+seeds[i+1])
+        seed_ranges.append(seed_range)
+
+    start_jobs(seed_ranges, mappings, procs, jobs, results)
+
+    val = report(procs, results)
+    print(val)
+
+if __name__ == '__main__':
+    with open('input5.txt') as f:
+        inputs = f.read()
+    solve_mp(inputs)
