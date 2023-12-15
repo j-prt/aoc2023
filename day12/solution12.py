3b9a96810c3d83837c64d1c8e04f79925ffcad2f
j-prt <peart.l.joe@gmail.com>
2023-12-15 13:35:57 -0800

    checkpoint day 12 part 2

--- day12/solution12.py
+++ day12/solution12.py
@@ -2,7 +2,7 @@
 
 # Puzzle 1
 from re import findall, sub
-from itertools import combinations, permutations
+from itertools import combinations
 
 with open('example.txt') as f:
     example = f.read()
@@ -10,17 +10,8 @@ with open('example.txt') as f:
 with open('input12.txt') as f:
     inputs = f.read()
 
-# def parse_inputs(inputs):
-#     lines = inputs.rstrip().split('\n')
-#     all_gears, all_sizes = [], []
-#     for record in lines:
-#         gear, sizes = record.split()
-#         sizes = [int(size) for size in sizes.split(',')]
-#         unknowns = list(finditer(r'[?]+', gear))
-#         knowns = list(finditer(r'[#]+', gear))
-
 def parse_inputs(inputs):
-    lines = inputs.rstrip().split('\n')
+    lines = inputs.rstrip().split('\n')[:10]
     records, templates = [], []
     for record in lines:
         gears, sizes = record.split()
@@ -33,6 +24,7 @@ def parse_inputs(inputs):
     return zip(records, templates)
 
 def get_perms(record, template):
+    exc_count = 0
     count = 0
     base_len = sum(len(item) for item in template)
     record_len = len(record)
@@ -55,6 +47,9 @@ def get_perms(record, template):
             current[idx] = '-'
         if findall('[#]+', ''.join(current)) == template:
             count += 1
+        if exc_count % 10_000_000 == 0:
+            print(exc_count, combo)
+        exc_count += 1
     return count
 
 def solve(inputs):
@@ -65,7 +60,59 @@ def solve(inputs):
     print(total)
 
 # Test
-solve(example)
+# solve(example)
 
 # Solve
-solve(inputs)
+# solve(inputs)
+
+
+# Puzzle 2
+
+# ohgod
+
+def parse_inputs_2(inputs):
+    lines = inputs.rstrip().split('\n')[:10]
+    records, templates = [], []
+    for record in lines:
+        gears, sizes = record.split()
+        template = ['#' * int(size) for size in sizes.split(',')]
+        template = template * 2
+        gears = gears +'?'+ gears
+        record = sub(r'[.]+', '-', gears)
+
+        records.append(record)
+        templates.append(template)
+
+    print(records[0], templates[0])
+    return zip(records, templates)
+
+
+# First (failed) attempt at solving part 2. The idea was that there
+# would be a constant multiplicative relationship between original
+# inputs and N expansions. This assumption held true on the example
+# set - by computing the 2x factor, I could extrapolate to 5x.
+# This unfortunately did not hold true for the actual inputs.
+
+def solve_2(inputs):
+    totally_total = 0
+    data1 = parse_inputs(inputs)
+    data2 = parse_inputs_2(inputs)
+    total1 = []
+    total2 = []
+    for record, template in data1:
+        num_perms = get_perms(record, template)
+        total1.append(num_perms)
+    for record, template in data2:
+        num_perms = get_perms(record, template)
+        total2.append(num_perms)
+    for one, two in zip(total1, total2):
+        rel = two / one
+        totally_total += one * rel**4
+        print(one, two, rel)
+    print(totally_total)
+
+# Test
+solve_2(example)
+
+# Solve
+solve_2(inputs)
