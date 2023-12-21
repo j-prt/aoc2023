e32e49471895bd18b351315b43c1ca3893ab21d7
j-prt <peart.l.joe@gmail.com>
2023-12-20 21:03:23 -0800

    solve day 15 part 2

--- day15/solution15.py
+++ day15/solution15.py
@@ -31,3 +31,50 @@ solve(example)
 
 # Solve
 solve(inputs)
+
+
+# Puzzle 2
+
+def HASHMAP(instr):
+    if '-' in instr:
+        code, length = instr.split('-')
+        add = False
+    else:
+        code, length = instr.split('=')
+        add = True
+
+    box_no = HASH(code)
+    if not boxes.get(box_no):
+        boxes[box_no] = {}
+
+    if add:
+        boxes[box_no][code] = length
+    else:
+        boxes[box_no].pop(code, None)
+
+
+def solve_2(inputs):
+    instructions = parse_inputs(inputs)
+
+    for instr in instructions:
+        HASHMAP(instr)
+
+    total = 0
+    for box in boxes:
+        num = box + 1
+        subtotal = 0
+        for idx, val in enumerate(boxes[box].values(), start=1):
+            subtotal += num * idx * int(val)
+        total += subtotal
+
+    print(total)
+
+boxes = {}
+
+# Test
+solve_2(example)
+
+boxes = {}
+
+# Solve
+solve_2(inputs)
